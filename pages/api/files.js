34e4916906cfd983e1f98841100154fc4cfdcb04
SeptAlfauzan <alfauzansepta@gmail.com>
2022-05-25 19:47:07 +0700

    delete unused API

--- pages/api/files.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { getPostsData, insert, save, supabaseGetFile } from "../../lib/posts"
-
-export default async function handler(req, res) {
-    const data = await insert();
-    res.status(200).json(data);
-}
